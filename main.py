9821d696c364d180ae16a1fdc836e586da4c2bf5
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-19 03:14:03 +0300

    add event to button

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+import os
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+
+class MyLayout(BoxLayout):
+
+    # create a box layout with a button
+    def __init__(self):
+        super().__init__()
+        self.button = Button(text='Press Me')
+        self.button.bind(on_press=self.new_label) # call the  new_label fuction on pressing the button
+        self.add_widget(self.button)
+
+    # create a label widget
+    def new_label(self, button):
+        self.label = Label(text='My new Label')
+        self.add_widget(self.label)
+        self.remove_widget(button) # remove the button after pressing it
+
 class MyApp(App):
-    
+
     def build(self):
-        
-        layout = BoxLayout(orientation='vertical')  # define layout
-        
-        label1 = Label(text='Hello World')
-        label2 = Label(text='Second button')  #add widgets
-        mybutton = Button(text='press me')
-
-        layout.add_widget(label1)
-        layout.add_widget(label2)  #initialize widgets on the layout
-        layout.add_widget(mybutton)
-
-        return layout
+        return MyLayout()
 
 if __name__ == '__main__':
     MyApp().run()
